34169c89c02fae41e6b47875f613e507b0eb6d73
Vinh Tran <officiallyvt@gmail.com>
2025-10-05 13:29:38 -0400

    added boilerplate for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom"
 import { useState } from 'react'
 import './App.css'
 
 function App() {
   return (
     <>
+      <Routes>
+        <Route path="/" element={<PAGE_ONE />} />
+        <Route path="/TWO" element={<PAGE_TWO />} />
+      </Routes>
     </>
   )
 }
