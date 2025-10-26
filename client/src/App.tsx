d1a8f55b8e704943ea687ba4a4f08f4ccc5e2432
Vinh Tran <officiallyvt@gmail.com>
2025-10-26 13:29:59 -0400

    updated pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom"
-import { useState } from 'react'
+import { useState } from 'react' // @ts-ignore
+import Home from './pages/Home' // @ts-ignore
+import Login from './pages/Login'
 import './App.css'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<PAGE_ONE />} />
-        <Route path="/TWO" element={<PAGE_TWO />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </>
   )
